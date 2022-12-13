decede9788bd1e770911b9d46e411a2c1cb8222b
oyinbra <onyinbra@gmail.com>
2022-12-13 12:37:54 +0100

    updated

--- manjaro-install.sh
+++ manjaro-install.sh
@@ -10,9 +10,11 @@ ln -sf ~/.dotfiles/latte-dock/.config/latte ~/.config
 ln -sf ~/.dotfiles/latte-dock/.config/lattedockrc ~/.config
 ln -sf ~/.dotfiles/neofetch-source/.neofetch-config2.conf ~/
 ln -sf ~/.dotfiles/themes/.themes ~/
-ln -sf ~/.dotfiles/zhistory/.zhistory ~/
 ln -sf ~/.dotfiles/zsh_aliases/.zsh_aliases ~/
 ln -sf ~/.dotfiles/zshrc-manjaro/.zshrc ~/
+# Enable color in nano editor for user
+ln -sf ~/.dotfiles/aurorae ~/.local/share/
+source ~/.zshrc
 
 
 sudo ln -sf ~/.dotfiles/neofetch-source/.neofetch-config2.conf /root
