a45c05a5c1c4f8dfa3915b903b1b8bebf6ab2b6a
oyinbra <onyinbra@gmail.com>
2023-01-14 09:31:47 +0100

    minor updates

--- post-arch.sh
+++ post-arch.sh
@@ -99,21 +99,6 @@ sudo ln -sf ~/.dotfiles/etc/paru.conf /etc/
 ####################################################################################
 ####################################################################################
 ##                                                                                ## 
-##                          Package Installation                                  ##
-##                                                                                ##
-####################################################################################
-####################################################################################
-
-# sudo pacman -Sy --noconfirm --needed latte-dock alacritty obs-studio gparted
-
-# sudo pacman -U obs-studio-28.1.2-2-x86_64.pkg.tar.zst brave-bin-1_1.45.133-1-x86_64.pkg.tar.zst pycharm-community-edition-2022.2.4-1-x86_64.pkg.tar.zst android-studio-2021.3.1.17-1-x86_64.pkg.tar.zst discord-0.0.21-2-x86_64.pkg.tar.zst uget-2.2.3-6-x86_64.pkg.tar.zst fluent-reader-bin-1.1.3-1-x86_64.pkg.tar.zst mailspring-1.10.7-1-x86_64.pkg.tar.zst bitwarden-2022.11.0-1-x86_64.pkg.tar.zst motrix-bin-1.6.11-1.1-x86_64.pkg.tar.zst freedownloadmanager-6.18.1.4920-1-x86_64.pkg.tar.zst google-chrome-108.0.5359.71-1-x86_64.pkg.tar.zst notion-app-enhanced-2.0.18-1.1-x86_64.pkg.tar.zst rust-1_1.65.0-1-x86_64.pkg.tar.zst paru-1.11.1-1-x86_64.pkg.tar.zst duf-0.8.1-2-x86_64.pkg.tar.zst btop-1.2.13-1-x86_64.pkg.tar.zst ksysguard-5.22.0-1-x86_64.pkg.tar.zst telegram-desktop-4.3.4-1-x86_64.pkg.tar.zst alacritty-0.11.0-2-x86_64.pkg.tar.zst onlyoffice-bin-7.2.1-1.1-x86_64.pkg.tar.zst libreoffice-fresh-7.4.3-2-x86_64.pkg.tar.zst gthumb-3.12.2-1-x86_64.pkg.tar.zst clutter-1.26.4-2-x86_64.pkg.tar.zst imath-3.1.6-1-x86_64.pkg.tar.zst webkit2gtk-2.38.2-2-x86_64.pkg.tar.zst neovim-symlinks-5-1-any.pkg.tar.zst rate-mirrors-bin-0.11.1-1-x86_64.pkg.tar.zst bat-0.22.1-1-x86_64.pkg.tar.zst firewalld-1.2.1-1-any.pkg.tar.zst python-pyqt5-5.15.7-3-x86_64.pkg.tar.zst nethogs-git-v0.8.7.r16.g80df004-1-x86_64.pkg.tar.zst
-
-# timeshift-bin-22.11.1-1-x86_64.pkg.tar.zst timeshift-autosnap-0.9-1-any.pkg.tar.zst
-
-
-####################################################################################
-####################################################################################
-##                                                                                ## 
 ##                         POWER MANAGEMENT Installation                          ##
 ##                                                                                ##
 ####################################################################################
