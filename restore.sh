c4864761a9b51849e1c733abd1b9d64285d65c7d
oyinbra <onyinbra@gmail.com>
2023-06-03 09:33:32 +0100

    💻 Single restore script

--- restore.sh
+++ restore.sh
@@ -7,8 +7,17 @@ echo "
 #############################################################################
 #############################################################################"
 
+# Restore Backup
+rsync -aAXv --ignore-times /run/media/$USER/Backup/home/$USER /home/
+
+# Restore pkg backup
+sudo rsync -aAXv --ignore-times /run/media/$USER/Backup/pkg/. /var/cache/pacman/pkg
+
+# Add backup partition to fstab
+echo "/dev/nvme0n1p4                            /Backup        btrfs   defaults                 0 0" | sudo tee --append /etc/fstab
+
 # Sync packages
-sudo pacman -Sy archlinux-keyring zoxide latte-dock neovim ksysguard zsh auto-cpufreq
+sudo pacman -Sy latte-dock zsh neovim zoxide ksysguard kitty firewalld kdeconnect git
 
 sudo systemctl enable --now firewalld
 sudo firewall-cmd --state
@@ -17,15 +26,9 @@ sudo firewall-cmd --permanent --add-service=https
 sudo firewall-cmd --reload
 sudo firewall-cmd --list-services
 
-# Restore Backup
-rsync -aAXv --delete /run/media/$USER/Backup/home/$USER /home/
-mkdir /home/$USER/Downloads
-echo 'XDG_DOWNLOAD_DIR="$HOME/Downloads"' >> ~/.config/user-dirs.dirs
-# Add backup partition to fstab
-echo "/dev/nvme0n1p4                            /Backup        btrfs   defaults                 0 0" | sudo tee --append /etc/fstab
-# Restore pkg backup
-sudo rsync -aAXv /run/media/$USER/Backup/pkg/. /var/cache/pacman/pkg
-# sudo rsync -aAXv /Backup/pkg/. /var/cache/pacman/pkg
+# firewalld plus KDE setup
+sudo firewall-cmd --permanent --zone=public --add-service=kdeconnect
+sudo firewall-cmd --reload
 
 # zsh shell configurations
 ln -sf ~/Dotfiles/zplug ~/.zplug
@@ -35,8 +38,6 @@ ln -sf ~/Dotfiles/p10k-user/.p10k.zsh ~/
 ln -sf ~/Dotfiles/neofetch-source/.neofetch-config2.conf ~/
 # latte-dock
 ln -sf ~/Dotfiles/.config/lattedockrc ~/.config
-# ln -sf ~/Dotfiles/latte-dock/.config/lattedockrc ~/.config/
-# ln -sf ~/Dotfiles/latte-dock/.config/latte ~/.config
 ln -sf ~/Dotfiles/.config/latte ~/.config
 ln -sf ~/Dotfiles/git/.gitignore_global ~/
 ln -sf ~/Dotfiles/icons/.icons ~/
@@ -56,13 +57,6 @@ ln -sf ~/Dotfiles/.local/share/plasma ~/.local/share
 mv ~/.local/share/konsole ~/.local/share/konsole.bak
 ln -sf ~/Dotfiles/.local/share/konsole/ ~/.local/share/
 
-# SDDM Themes
-sudo ln -sf ~/Dotfiles/usr/share/sddm/themes/Ocean /usr/share/sddm/themes/
-# Neovim config
-sudo ln -sf /home/$USER/Workspace/nvim /root/.config/
-# Paru config
-sudo ln -sf /home/$USER/Dotfiles/etc/paru.conf /etc/
-
 echo "
 #############################################################################
 #############################################################################
@@ -72,17 +66,21 @@ echo "
 #############################################################################
 #############################################################################"
 
+sudo ln -sf /home/$USER/Dotfiles/etc/paru.conf /etc/
+sudo ln -sf /home/$USER/Workspace/nvim /root/.config/
+sudo ln -sf ~/Dotfiles/usr/share/sddm/themes/Ocean /usr/share/sddm/themes/
 sudo ln -sf /home/$USER/Dotfiles/.config/zsh/.zshrc /root
 sudo ln -sf /home/$USER/Dotfiles/p10k-root/.p10k.zsh /root/
 sudo ln -sf /home/$USER/Dotfiles/.config/zsh /root/.config
 sudo ln -sf /home/$USER/Dotfiles/neofetch-source/.neofetch-config2.conf /root/.config
 sudo ln -sf /home/$USER/.zshenv /root
+sudo mkdir -p /root/.local/share
+sudo rm -rf /root/.local/share/nvim
 sudo ln -sf /home/$USER/.local/share/nvim /root/.local/share/
 sudo ln -sf /home/$USER/Dotfiles/neofetch-source/.neofetch-config2.conf /root
 sudo ln -sf /home/$USER/Dotfiles/neofetch-ascii/usr/bin/neofetch /usr/bin
 
 # enable auto-cpufreq
-sudo systemctl enable --now auto-cpufreq.service
 sudo systemctl mask power-profiles-daemon.service
 sudo ln -sf /home/$USER/Dotfiles/auto-cpufreq/etc/auto-cpufreq.conf /etc
 # stop the no watchdog message on reboot
@@ -92,4 +90,3 @@ sudo systemctl enable --now cronie.service
 echo "DONE"
 echo "Refresh zshrc"
 source ~/.zshrc
-
