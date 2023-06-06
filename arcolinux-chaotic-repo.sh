6a75a73d7d3b010948b4208d9d344e7dd0468fc9
oyinbra <onyinbra@gmail.com>
2023-06-06 13:37:31 +0100

    💻 Merged Arcolinux and Chaotic mirror repo settings as one

--- arcolinux-chaotic-repo.sh
+++ arcolinux-chaotic-repo.sh
@@ -29,4 +29,13 @@ Include = /etc/pacman.d/arcolinux-mirrorlist
 [arcolinux_repo_xlarge]
 SigLevel = Optional TrustedOnly
 Include = /etc/pacman.d/arcolinux-mirrorlist' | sudo tee --append /etc/pacman.conf
+
+######################################################################################################################
+
+sudo pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+sudo pacman-key --lsign-key FBA220DFC880C036
+sudo pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+echo '
+[chaotic-aur]
+Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee --append /etc/pacman.conf
 sudo pacman -Sy
