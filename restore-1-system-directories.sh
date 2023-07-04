f089b8e4cda9277af825a375b8b3609bda8b9d7b
oyinbra <onyinbra@gmail.com>
2023-07-04 15:47:25 +0100

    💻 removed bin and share from backup

--- restore-1-system-directories.sh
+++ restore-1-system-directories.sh
@@ -3,18 +3,16 @@
 # System Directory Restores
 directories=(
    "/root"
-   "/usr/bin"
-   "/usr/share"
+   "/usr/share/sddm/themes"
    "/var/cache/pacman/pkg"
    "/var/spool/cron"
 )
 
-# Creating a backup of bin and share directory
-sudo cp -R /usr/bin /usr/bin.bak
-sudo cp -R /usr/share /usr/share.bak
+# Creating a backup of bin and share diretory
+# sudo cp -R /usr/share /usr/share.bak
 
 for directory in "${directories[@]}"; do
-    if [[ "$directory" == "/usr/bin" || "$directory" == "/usr/share" ]]; then
+    if [[ "$directory" == "/usr/share/sddm/themes" ]]; then
         sudo rsync -aAXv /run/media/$USER/Backup/"$directory"/ "$directory"
     else
         sudo rsync -aAXv --delete --ignore-times /run/media/$USER/Backup/"$directory"/ "$directory"
