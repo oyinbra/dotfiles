e7a82b7fa348cff8a9e5a7dccae800d6e2bbb112
Onyinbrakeme  Kelvin Ogbe <35059691+Oyinbra@users.noreply.github.com>
2023-10-31 07:47:50 +0100

    ✨ added script to restore encrypted partition

---
+++ restoreEncryptedBackupPartition.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create a mount point
+sudo mkdir -p /Backup
+
+# Add an entry to /etc/crypttab to automatically unlock the encrypted partition during boot
+echo "encrypted_backup /dev/nvme0n1p5 none" | sudo tee -a /etc/crypttab
+
+# Add an entry to /etc/crypttab to automatically unlock the encrypted partition during boot
+
+echo "/dev/mapper/encrypted_backup /Backup btrfs defaults 0 0" | sudo tee -a /etc/fstab
+
+# Optionally, set permissions on the mount point to allow regular users to access it
+sudo chmod 777 /Backup
+# Replace oyinbra:oyinbra with your own username
+sudo chown -R oyinbra:oyinbra /Backup
