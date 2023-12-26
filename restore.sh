7ca8cdce0528d68a88f7de7fb87c13811e172579
Onyinbrakeme Kelvin Ogbe <35059691+Oyinbra@users.noreply.github.com>
2023-12-26 20:13:53 +0100

    ✨ refactored code

--- restore.sh
+++ restore.sh
@@ -16,6 +16,15 @@ EOF
 source $(dirname "$0")/modules/library.sh
 clear
 
+confirm_execution ./sysDirectories.sh
+confirm_execution ./arcolinuxRepos.sh
+confirm_execution ./paruInstall.sh
+confirm_execution ./yayInstall.sh
+confirm_execution ./myPackages.sh
+confirm_execution ./encryptedBackupPartition.sh
+confirm_execution ./homeRestore.sh
+confirm_execution ./symlinks.sh
+
 cat << "EOF"
 ▄▖▄▖▄▖▄▖▄▖▄▖  ▄▖▄▖▖▖  ▄▖▄▖▖ ▖▄▖▄▖▄▖
 ▙▘▙▖▐ ▌▌▙▘▙▖  ▗▘▚ ▙▌  ▌ ▌▌▛▖▌▙▖▐ ▌
@@ -23,3 +32,4 @@ cat << "EOF"
 
 EOF
 confirm_execution ./zshConfig.sh
+
