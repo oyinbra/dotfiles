d06f1f84c410c499712168ce250ca6777ae6e51c
Onyinbrakeme Kelvin Ogbe <35059691+Oyinbra@users.noreply.github.com>
2023-12-30 21:25:46 +0100

    🎨 switched pkg to yay

--- zsh/aliases/pkgMgrAliases.zsh
+++ zsh/aliases/pkgMgrAliases.zsh
@@ -15,71 +15,68 @@ alias deploy="yay -S"
 # Remove a package using (AUR helper)
 alias purge="yay -R"
 
-# Update the package list with 'sudo pacman'
-alias update="sudo pacman -Sy"
+# Update the package list with 'yay'
+alias update="yay -Sy"
 
-# Upgrade installed packages with 'sudo pacman'
-alias upgrade="sudo pacman -Syu"
+# Upgrade installed packages with 'yay'
+alias upgrade="yay"
 
-# Refresh the file database with 'sudo pacman'
-alias refresh="sudo pacman -Fy"
+# Refresh the file database with 'yay'
+alias refresh="yay -Fy"
 
 # Check if a package in install
-alias check="pacman -Q | grep"
+alias check="yay -Q | grep"
 
 # Sync Packages
 alias align="yay -Syy"
 
-# Search for packages using 'sudo pacman'
-alias search="sudo pacman -Ss"
+# Search for packages using 'yay'
+alias search="yay -Ss"
 
-# Query installed packages with 'pacman'
-alias query="pacman -Qs"
+# Query installed packages with 'yay'
+alias query="yay -Qs"
 
-# Install a package from a file using 'sudo pacman'
-alias pacfile="sudo pacman -S --needed - <"
+# Install a package from a file using 'yay'
+alias pacfile="yay -S --needed - <"
 
-# Downgrade a package using 'sudo pacman'
-alias downgrade="sudo pacman -Suu"
+# Downgrade a package using 'yay'
+alias downgrade="yay -Suu"
 
 # Upgrade/Install a package using package file
-alias pkg="sudo pacman -U"
+alias pkg="yay -U"
 
-# Clean package cache with 'sudo pacman'
-alias pacclean="sudo pacman -Sc"
+# Clean package cache with 'yay'
+alias pacclean="yay -Sc"
 
-# Remove old packages from the cache with 'sudo pacman'
-alias pacalien="sudo pacman -c"
+# Remove old packages from the cache with 'yay'
+alias pacalien="yay -c"
 
-# Clean all package cache with 'sudo pacman'
-alias paccache="sudo pacman -Scc"
+# Clean all package cache with 'yay'
+alias paccache="yay -Scc"
 
 # List cached packages in /var/cache/pacman/pkg
 alias paclistpkg="ls /var/cache/pacman/pkg | less"
 
-# List orphan packages with 'sudo pacman'
-alias paclistorphan="sudo pacman -Qtdq"
+# List orphan packages with 'yay'
+alias paclistorphan="yay -Qtdq"
 
-# Remove orphan packages with 'sudo pacman'
-alias pacremorphan="sudo pacman -R $(pacman -Qtdq)"
+# Remove orphan packages with 'yay'
+alias pacremorphan="yay -R $(yay -Qtdq)"
 
 # Display package dependencies recursively with 'pactree'
 alias pactree.r="pactree -r"
 
-# Remove a package and its unneeded dependencies with 'sudo pacman'
-alias pacrs="sudo pacman -Rs"
+# Remove a package and its unneeded dependencies with 'yay'
+alias pacrs="yay -Rs"
 
-# Remove a package and its dependencies with 'sudo pacman'
-alias pacrns="sudo pacman -Rns"
+# Remove a package and its dependencies with 'yay'
+alias pacrns="yay -Rns"
 
-# Find the package providing a specific file with 'pacman -F'
-alias pacfind="pacman -F"
+# Find the package providing a specific file with 'yay -F'
+alias pacfind="yay -F"
 
-# Unlock pacman database if stuck by removing the lock file
+# Unlock yay database if stuck by removing the lock file
 alias pacunlock="sudo rm -rf /var/lib/pacman/db.lck"
-
-# Set mirrors for Arch Linux (Jaro) using 'sudo pacman-mirrors'
-alias pacmanjaro="sudo pacman-mirrors --fasttrack"
 
 # View last installed packages
 alias paclast='if [ -f /var/log/pacman.log ]; then expac --timefmt="%Y-%m-%d %T" "%l|%-30n|%-15d" $(zcat /var/log/pacman.log* 2>/dev/null | awk "/^\[.*\] installed/ {print \$1}") | sort | tail -n 100; else echo "Pacman log file not found or not in gzip format"; fi'
