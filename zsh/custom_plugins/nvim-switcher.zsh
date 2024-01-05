803fb0bb83f8993eb91ac5a711aa878b14423fa4
Onyinbrakeme Kelvin Ogbe <35059691+Oyinbra@users.noreply.github.com>
2024-01-05 20:35:30 +0100

    ✨ bind ns to switch neovim

--- zsh/custom_plugins/nvim-switcher.zsh
+++ zsh/custom_plugins/nvim-switcher.zsh
@@ -6,14 +6,18 @@
 # █  ██ █   █ █ █ █  █  █   █     █ ██ ██  █   █  █   █ █ █   █ █
 # █   █ ███ ███  █  ███ █   █   ███ █   █ ███  █  ███ █ █ ███ █ █
 
+#------------------------------------------------------
 # Name on folder in .config directory
+#------------------------------------------------------
 alias lazy="NVIM_APPNAME=LazyVim nvim"
 alias kick="NVIM_APPNAME=KickStart nvim"
 alias chad="NVIM_APPNAME=NvChad nvim"
 alias astro="NVIM_APPNAME=AstroNvim nvim"
 
-# vs function to switch between neovim configuration
-function vs() {
+#------------------------------------------------------
+# ns function to switch between neovim configuration
+#------------------------------------------------------
+function ns() {
   items=("default" "KickStart" "LazyVim" "NvChad" "AstroNvim")
   config=$(printf "%s\n" "${items[@]}" | fzf --prompt=" Neovim Config  " --height=~50% --layout=reverse --border --exit-0)
   if [[ -z $config ]]; then
@@ -24,6 +28,6 @@ function vs() {
   fi
   NVIM_APPNAME=$config nvim $@
 }
-# Bind ctrl+y to vs for neovim switcher
-bindkey "^Y" vs
+# Bind ctrl+y to ns for neovim switcher
+bindkey "^Y" ns
 
