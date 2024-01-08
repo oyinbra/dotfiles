5f235959a3588b8d244a4761c4cacdccbb2e6690
Onyinbrakeme Kelvin Ogbe <35059691+Oyinbra@users.noreply.github.com>
2024-01-08 22:59:04 +0100

    📖 Adding or updating comments in the code

--- modules/killMechvibes.sh
+++ modules/killMechvibes.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-sleep 10
-killall mechvibes
 
+# This line pauses the script execution for 10 seconds.
+sleep 10
+
+# This line terminates (kills) all processes with the name 'mechvibes'.
+killall mechvibes
