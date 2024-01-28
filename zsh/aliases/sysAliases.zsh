0df431ec08bfa5bb2b4e9f40cf16c992a7357703
Onyinbrakeme Kelvin Ogbe <35059691+Oyinbra@users.noreply.github.com>
2024-01-28 21:31:43 +0100

    ✨ add alias to disable Meta

--- zsh/aliases/sysAliases.zsh
+++ zsh/aliases/sysAliases.zsh
@@ -16,6 +16,7 @@ alias metalist="qdbus org.kde.kglobalaccel /component/kwin org.kde.kglobalaccel.
 alias metakrunner='kwriteconfig5 --file ~/.config/kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.krunner,/App,,toggleDisplay" && metareload'
 alias metalauncher='kwriteconfig5 --file ~/.config/kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.plasmashell,/PlasmaShell,org.kde.PlasmaShell,activateLauncherMenu" && metareload'
 alias metaexposeall='kwriteconfig5 --file ~/.config/kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.kglobalaccel,/component/kwin,org.kde.kglobalaccel.Component,invokeShortcut,ExposeAll" && metareload'
+alias metadisable='kwriteconfig5 --file ~/.config/kwinrc --group ModifierOnlyShortcuts --key Meta ""'
 # Windows overview
 alias metaoverview='kwriteconfig5 --file ~/.config/kwinrc --group ModifierOnlyShortcuts --key Meta "org.kde.kglobalaccel,/component/kwin,org.kde.kglobalaccel.Component,invokeShortcut,Overview" && metareload'
 # Check swapp value 
